d649f89f348ef70eade66b927de60aa052f6c316
Grazi <grazielidsm2@gmail.com>
2024-08-12 00:17:55 -0300

    beecrowd 01

---
+++ Soma_Beecrowd_ex02.c
@@ -0,0 +1,34 @@
+/*
+https://judge.beecrowd.com/pt/problems/view/1001
+
+*/
+
+/*
+#include <stdio.h>
+
+int main ()
+{
+    int Valor1, Valor2, Soma;
+
+    scanf("%d" "%d", &Valor1, &Valor2);
+    Soma = Valor1 +Valor2;
+    printf("X = %d", Soma);
+
+    return 0;
+}
+*/
+
+#include <stdio.h>
+
+int main ()
+{
+    int A, B, soma;
+
+    scanf("%d""%d", &A, &B);
+    soma=A+B;
+    printf("X = %d", soma);
+
+    return 0;
+}
+
+// os dois programas seriam aceitos de havesse printf("X = %d\n", soma); na ultima linha.
